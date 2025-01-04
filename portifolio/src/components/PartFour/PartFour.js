def909e0e52a699f52bda207d6e32121f414fd7c
Luiz Henrique Pasin Laikovski <luizlaikovski@gmail.com>
2025-01-04 17:57:38 -0300

    Mais uma seção

--- portifolio/src/components/PartFour/PartFour.js
+++ portifolio/src/components/PartFour/PartFour.js
@@ -1,12 +1,53 @@
 import './PartFour.css'
 import Risco from '../Risco/Risco';
+import RiscoH from '../RiscoH/RiscoH';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCss, faCss3, faCss3Alt, faHtml5, faJs, faPython } from '@fortawesome/free-brands-svg-icons';
+
 
 function PartFour() {
     return (
         <>
             <section className="Partf">
-                <h1>POPOPOP</h1>
-                <Risco altura='45' largura='2' />
+                <main className="division">
+                    <div className="habilits">
+                        <h1 style={{marginTop: '10px'}}>Paroquia São José Operário</h1>
+                        <RiscoH largura='20' grossura='0.8' margens='20'/>
+                        <p>Esse é um projeto teve inicio no final do ano de 2024. Ele tem como objetivo apresentar os ministério da paróquia, para os fiéis que a frequentam. Tecnologias Usadas: </p>
+                        <div>
+                            <FontAwesomeIcon icon={faHtml5} className="icon"/>
+                            <FontAwesomeIcon icon={faCss3Alt} className="icon"/>
+                            <FontAwesomeIcon icon={faJs} className="icon"/>
+                        </div>
+                    </div>
+                    <div className="habilits">
+                        <h1 style={{marginTop: '10px'}}>Projeto com Flask</h1>
+                        <RiscoH largura='20' grossura='0.8' margens='20'/>
+                        <p>Esse é um Projeto onde faço ligação de um Banco de Dados com FrontEnd usando o Python e a biblioteca Flask. Tecnologias Usadas: </p>
+                        <div>
+                            <FontAwesomeIcon icon={faPython} className="icon" />
+                            <FontAwesomeIcon icon={faHtml5} className="icon" />
+                            <FontAwesomeIcon icon={faCss3Alt} className="icon" />
+                        </div>
+                    </div>
+                    <div className="habilits">
+                        <h1 style={{marginTop: '10px'}}>Sistema de Estoque</h1>
+                        <RiscoH grossura='0.8' margens='20' largura='20'/>
+                        <p>Nesse pequeno projeto criei um sistema de estoque. Onde possui a funcionabilidade de Adcionar, Listar, Editar(preço, quantidade e nome) e deletar o produto ou apenas uma quantidade </p>
+                        <FontAwesomeIcon icon={faPython} className="icon" />
+                    </div>
+                    <div className="habilits">
+                        <h1 style={{marginTop: '10px'}}>Testes</h1>
+                        <RiscoH largura='20' grossura='0.8' margens='20'/>
+                    </div>
+                    <div className="habilits">
+                        <h1 style={{marginTop: '10px'}}>Testes</h1>
+                        <RiscoH largura='20' grossura='0.8' margens='20'/></div>    
+                    <div className="habilits">
+                        <h1 style={{marginTop: '10px'}}>Testes</h1>
+                        <RiscoH largura='20' grossura='0.8' margens='20'/>
+                    </div>
+                </main>
             </section>
         </>
     );
---
+++ portifolio/src/components/RiscoH/RiscoH.js
@@ -0,0 +1,19 @@
+import { useState } from "react";
+
+function RiscoH({largura, grossura, margens}) {
+    let [size, setSize] = useState({width: 100, height: 100})
+
+    return(
+        <> 
+            <div style={{
+                width: `${largura}dvw`,
+                height: `${grossura}px`,
+                backgroundColor: 'black',
+                marginTop: `${margens}px`,
+                marginBottom: `${margens}px`
+            }}></div>
+        </>
+    );
+}
+
+export default RiscoH
