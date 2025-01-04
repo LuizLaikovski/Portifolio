382b68815d31dba90f4ea07f69509dc55f22acd9
Luiz Henrique Pasin Laikovski <luizlaikovski@gmail.com>
2025-01-04 13:45:05 -0300

    ...

--- portifolio/src/components/PartFour/PartFour.js
+++ portifolio/src/components/PartFour/PartFour.js
@@ -5,7 +5,7 @@ function PartFour() {
     return (
         <>
             <section className="Partf">
-                
+                <h1>POPOPOP</h1>
                 <Risco altura='45' largura='2' />
             </section>
         </>
