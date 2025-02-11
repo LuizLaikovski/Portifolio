fac87dbd7c3728aab397d82c567af0f3d4b745e9
Luiz Henrique Pasin Laikovski <luizlaikovski@gmail.com>
2025-02-11 16:38:51 -0300

    na produção da navbar responsivaaa

--- portifolio/src/components/Faixa/Faixa.js
+++ portifolio/src/components/Faixa/Faixa.js
@@ -15,6 +15,11 @@ function Faixa() {
         <>
             <header className="Faixa">
                 <a className="LOGOO" href="https://github.com/luizlaikovski">Luiz Laikovski | Dev</a>
+                <div className='mobile-menu'>
+                    <div className='line'></div>
+                    <div className='line'></div>
+                    <div className='line'></div>
+                </div>
                 <ul className="NavBar">
                     <li onClick={() => scrollToElements('SobreMim')}>Sobre Mim</li>
                     <li onClick={() => scrollToElements('Skills')}>Minhas Skill's</li>
@@ -27,5 +32,49 @@ function Faixa() {
         </>
     );
 }
+
+class MobileNavBar {
+    constructor(mobileMenu, navList, navLinks) {
+        this.mobileMenu = document.querySelector(mobileMenu);
+        this.navList = document.querySelector(navList);
+        this.navLinks = document.querySelectorAll(navLinks);
+        this.activeClass = "active";
+
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+
+    animateLinks() {
+        this.navLinks.forEach((link) => {
+            link.style.animation
+            ? (link.style.animation = "")
+            : (link.style.animation = "navLinkFade 0.5s ease forwards 0.3s");
+        });
+    }
+
+    handleClick() {
+        this.navList.classList.toggle(this.activeClass);
+        this.animateLinks();
+    }
+
+    addClickEvent() {
+        this.mobileMenu.addEventListener("click", this.handleClick);
+    }
+
+    init () {
+        if (this.mobileMenu) {
+            this.addClickEvent();
+        }
+
+        return this;
+    }
+}
+
+const mobileNavBar = new MobileNavBar(
+    ".mobile-menu",
+    ".NavBar",
+    ".NavBar li"
+);
+mobileNavBar.init();
 
 export default Faixa
